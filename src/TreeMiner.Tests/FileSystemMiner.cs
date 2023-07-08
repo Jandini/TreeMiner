09800a5a563bc83a2735fdd2d72906fc4713ae91
Matt Janda <matt.janda.kingston@gmail.com>
2023-07-08 19:21:32 +0100

    Add GetRootArtifact method to generic tree miner (#4)

--- src/TreeMiner.Tests/FileSystemMiner.cs
+++ src/TreeMiner.Tests/FileSystemMiner.cs
@@ -2,11 +2,11 @@
 {
 
     public class FileSystemMiner<TTreeArtifact> : GenericTreeMiner<TTreeArtifact, FileSystemInfo, FileInfo, DirectoryInfo> where TTreeArtifact : ITreeArtifact, new()
-    {
-        public IEnumerable<TTreeArtifact> DigFileSystem(TTreeArtifact dirArtifact, ArtifactType artifactType = ArtifactType.All, DepthOption depthMode = DepthOption.Deep, Func<TTreeArtifact, IEnumerable<FileSystemInfo>, bool>? onDirArtifact = null, Func<TTreeArtifact, bool>? onFileArtifact = null, Func<Exception, bool>? onException = null) 
-            => DigArtifacts(dirArtifact, (dirInfo) => dirInfo.GetFileSystemInfos(), artifactType, depthMode, onDirArtifact, onFileArtifact, onException);
+    {        
+        public IEnumerable<TTreeArtifact> GetFileSystemArtifacts(TTreeArtifact dirArtifact, ArtifactType artifactType = ArtifactType.All, DepthOption depthMode = DepthOption.Deep, Func<TTreeArtifact, IEnumerable<FileSystemInfo>, bool>? onDirArtifact = null, Func<TTreeArtifact, bool>? onFileArtifact = null, Func<Exception, bool>? onException = null) 
+            => GetArtifacts(dirArtifact, (dirInfo) => dirInfo.GetFileSystemInfos(), artifactType, depthMode, onDirArtifact, onFileArtifact, onException);
 
-        public IEnumerable<TTreeArtifact> DigFileSystem(TTreeArtifact dirArtifact, ArtifactType artifactType = ArtifactType.All, DepthOption depthMode = DepthOption.Deep, List<Exception>? exceptionAggregate = null)
-            => DigArtifacts(dirArtifact, (dirInfo) => dirInfo.GetFileSystemInfos(), artifactType, depthMode, exceptionAggregate);
+        public IEnumerable<TTreeArtifact> GetFileSystemArtifacts(TTreeArtifact dirArtifact, ArtifactType artifactType = ArtifactType.All, DepthOption depthMode = DepthOption.Deep, List<Exception>? exceptionAggregate = null)
+            => GetArtifacts(dirArtifact, (dirInfo) => dirInfo.GetFileSystemInfos(), artifactType, depthMode, exceptionAggregate);
     }
 }
