2183d4835394467e39e1872bc4bbb37cdf922ee4
Matt Janda <matt.janda.kingston@gmail.com>
2023-07-08 23:11:09 +0100

    Add artifact excavator interface

---
+++ src/TreeMiner/IArtifactExcavator.cs
@@ -0,0 +1,9 @@
+﻿namespace TreeMiner
+{
+    public interface IArtifactExcavator<TTreeArtifact, TBaseArtifact, TFileArtifact, TDirArtifact> where TTreeArtifact : ITreeArtifact, new() where TFileArtifact : class, TBaseArtifact where TDirArtifact : class, TBaseArtifact
+    {
+        public bool OnDirArtifact(TTreeArtifact dirArtifact, IEnumerable<TBaseArtifact> dirContent);
+        public bool OnFileArtifact(TTreeArtifact fileArtifact);
+        public bool OnException(Exception exception);
+    }
+}
