f98f86a11572ac31af75abd10c7b5effae844ef3
Matt <matt.janda.kingston@gmail.com>
2025-01-26 14:36:42 +0000

    Add file system miner and file system excavator

---
+++ src/TreeMiner.FileSystem/FileSystemExcavator.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace TreeMiner.FileSystem
+{
+    internal class FileSystemExcavator : ITreeExcavator<FileSystemArtifact, FileSystemInfo, FileInfo, DirectoryInfo>
+    {
+        private readonly CancellationToken _cancellationToken;
+
+        public FileSystemExcavator(CancellationToken cancellationToken = default)
+        {
+            _cancellationToken = cancellationToken;
+        }
+        
+        public IEnumerable<FileSystemInfo> GetArtifacts(DirectoryInfo dirArtifact)
+        {
+            return dirArtifact.GetFileSystemInfos();
+        }
+
+        public bool OnDirArtifact(FileSystemArtifact dirArtifact, IEnumerable<FileSystemInfo> dirContent)
+        {
+            return !_cancellationToken.IsCancellationRequested;
+        }
+
+        public bool OnException(ArtifactException<FileSystemInfo> exception)
+        {
+            throw exception;
+        }
+
+        public bool OnFileArtifact(FileSystemArtifact fileArtifact)
+        {
+            return !_cancellationToken.IsCancellationRequested;
+        }
+    }
+}
--- src/TreeMiner.FileSystem/FileSystemMiner.cs
+++ src/TreeMiner.FileSystem/FileSystemMiner.cs
@@ -1,7 +1,13 @@
 ﻿namespace TreeMiner.FileSystem
 {
-    public class FileSystemMiner<TTreeArtifact> where TTreeArtifact : IFileSystemArtifact, new()
+    public class FileSystemMiner<TTreeArtifact> : GenericTreeMiner<TTreeArtifact, FileSystemInfo, FileInfo, DirectoryInfo> where TTreeArtifact : IFileSystemArtifact, new()
     {
-        
+
+        public static IEnumerable<IFileSystemArtifact> GetArtifacts(string path, CancellationToken cancellationToken = default)
+        {
+            var fsm = new FileSystemMiner<FileSystemArtifact>();
+            var root = fsm.GetRootArtifact(new DirectoryInfo(path));
+            return fsm.GetArtifacts(root, new FileSystemExcavator(cancellationToken));            
+        }        
     }
 }
